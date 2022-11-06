40873139f6dc508ddb13f989752f0bba16fa5f25
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-11-06 18:54:30 +0700

    Update Tags.lua

--- Tags.lua
+++ Tags.lua
@@ -1,24 +1,4 @@
 return {
-  ["IIIlllIlIIlllIlIlIIl"] = {
-    TagText = "COMET SCRIPTER",
-    TagColor = Color3.fromRGB(255,65,65)
-  },
-  ["uihuiiiiiiiiiiiiiih"] = {
-    TagText = "COMET OWNER",
-    TagColor = Color3.fromRGB(0,255,200)
-  },
-  ["Ishid1234t"] = {
-    TagText = "NOBOLINE OWNER",
-    TagColor = Color3.fromRGB(255,255,0)
-  },
-  ["DepredationGamin_g"] = {
-    TagText = "Ham135IsDaddy",
-    TagColor = Color3.fromRGB(0,115,255)
-  },
-  ["ihads_Xwithyoulol"] = {
-    TagText = "Cumslut",
-    TagColor = Color3.fromRGB(255,0,255)
-  },
   ["AstralVaspe"] = {
     TagText = "NIGHTBED OWNER",
     TagColor = Color3.fromRGB(255,95,95)
@@ -27,12 +7,12 @@ return {
     TagText = "PRISM USER",
     TagColor = Color3.fromRGB(255,95,95)
   },
+  ["forgornameboblus"] = {
+    TagText = "YZFloppa",
+    TagColor = Color3.fromRGB(255,95,95)
+  },
   ["xAom95"] = {
     TagText = "NIGHTBED OWNER",
     TagColor = Color3.fromRGB(255,95,95)
-  },
-  ["sss_6475487"] = {
-    TagText = "COMET SCRIPTER",
-    TagColor = Color3.fromRGB(65,65,255)
   }
 }
