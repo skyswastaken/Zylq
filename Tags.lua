cede7e405b31fb3443ed30af93e84833cfcb2f18
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-11-10 16:52:02 +0700

    Update Tags.lua

--- Tags.lua
+++ Tags.lua
@@ -11,6 +11,10 @@ return {
     TagText = "YZFloppa",
     TagColor = Color3.fromRGB(255,95,95)
   },
+  ["Monia_9266"] = {
+    TagText = "NIGHTBED OWNER",
+    TagColor = Color3.fromRGB(255,95,95)
+  },
   ["xAom95"] = {
     TagText = "NIGHTBED OWNER",
     TagColor = Color3.fromRGB(255,95,95)
