2f4b3704339c704cc16301d5326867b2c2ceac3c
Monia <102583640+NTDCore@users.noreply.github.com>
2023-03-20 10:32:26 +0700

    update Tags.lua

--- Tags.lua
+++ Tags.lua
@@ -1,34 +1,34 @@
 return {
-  ["AstralVaspe"] = {
-    TagText = "NIGHTBED OWNER",
-    TagColor = Color3.fromRGB(255,95,95)
-  },
-  ["PrismUserz"] = {
-    TagText = "PRISM USER",
-    TagColor = Color3.fromRGB(255,95,95)
-  },
-  ["forgornameboblus"] = {
-    TagText = "YZFloppa",
-    TagColor = Color3.fromRGB(255,95,95)
-  },
-  ["Monia_9266"] = {
-    TagText = "NIGHTBED OWNER",
-    TagColor = Color3.fromRGB(255,95,95)
-  },
-  ["xAom95"] = {
-    TagText = "NIGHTBED OWNER",
-    TagColor = Color3.fromRGB(255,95,95)
-  }.
-  ["MoniaReturn"] = {
-  	TagText = "VAPE OWNER",
+	["AstralVaspe"] = {
+		TagText = "NIGHTBED OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-  },
-  ["HellBladeRelease"] = {
+	},
+	["PrismUserz"] = {
+		TagText = "PRISM USER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["forgornameboblus"] = {
+		TagText = "YZFloppa",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["Monia_9266"] = {
+		TagText = "NIGHTBED OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["xAom95"] = {
+		TagText = "NIGHTBED OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["MoniaReturn"] = {
 		TagText = "VAPE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-  },
-  ["YZFloppa_alt"] = {
+	},
+	["HellBladeRelease"] = {
 		TagText = "VAPE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-  }
+	},
+	["YZFloppa_alt"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	}
 }
