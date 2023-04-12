d31d70da27c0985d8de57de5ff7c2f42d6cbef5d
Monia <102583640+NTDCore@users.noreply.github.com>
2023-04-12 13:31:31 +0700

    Update Tags.lua

--- Tags.lua
+++ Tags.lua
@@ -47,6 +47,10 @@ return {
                 TagText = "VAPE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
         },
+	["XylexIsCanada"] = {
+                TagText = "LAZERWARE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+        },
         ["MessiMessiREAL"] = {
                 TagText = "VAPE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
