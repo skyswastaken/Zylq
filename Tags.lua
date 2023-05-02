9590b04ff74240fcd5211f5670b8d3c2044b3ba9
Monia <102583640+NTDCore@users.noreply.github.com>
2023-05-02 15:06:27 +0700

    update Tags.lua

--- Tags.lua
+++ Tags.lua
@@ -36,27 +36,27 @@ return {
 		TagColor = Color3.fromRGB(255,95,95)
 	},
 	["qwertyui_bigmac"] = {
-                TagText = "VAPE OWNER",
+		TagText = "VAPE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-        },
+	},
 	["table0x768"] = {
-                TagText = "VAPE OWNER",
+		TagText = "VAPE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-        },
+	},
 	["XylexIsCanadain"] = {
-                TagText = "VAPE OWNER",
+		TagText = "VAPE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-        },
+	},
 	["table0x472"] = {
-                TagText = "RISE OWNER",
+		TagText = "RISE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-        },
-	["MaxlaserTechOnTop1"] = {
-                TagText = "LAZERWARE OWNER",
+	},
+	["MaxlaserTechOnTop5"] = {
+		TagText = "ZEPHYR OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-        },
-        ["MessiMessiREAL"] = {
-                TagText = "VAPE OWNER",
+	},
+	["MessiMessiREAL"] = {
+		TagText = "VAPE OWNER",
 		TagColor = Color3.fromRGB(255,95,95)
-        }
+	}
 }
