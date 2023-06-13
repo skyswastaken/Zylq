0c1d99e019b9e455e778e0b4bae55a6a1344731d
MaxlaserTech <104206888+IlikeScript1234@users.noreply.github.com>
2023-06-13 21:49:34 +0700

    Update HWIDTags.lua

--- HWIDTags.lua
+++ HWIDTags.lua
@@ -1,9 +1,9 @@
 return {
-	5f4258e3-61b1-4bf6-a5b4-00d89e752107 = {
+	[5f4258e3-61b1-4bf6-a5b4-00d89e752107] = {
 		TagText = "YZFloppa",
 		TagColor = Color3.fromRGB(255,95,95)
 	},
-	E54674BC-BF52-4E3C-B5A5-0CE24D4BFB70 = {
+	[E54674BC-BF52-4E3C-B5A5-0CE24D4BFB70] = {
 		TagText = "Maxlaser",
 		TagColor = Color3.fromRGB(255,95,95)
 	},
